96911b9e180b56acfde1d6d0924ee9034e9f45c4
Razvan Bordeanu <razvan.bordeanu@misys.com>
2015-09-30 18:35:21 +0300

    bootstrapping

---
+++ src/main/java/com/kassandra/Bootstrap.java
@@ -0,0 +1,9 @@
+package com.kassandra;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "main", urlPatterns = {"/main"})
+public class Bootstrap extends HttpServlet {
+
+}
