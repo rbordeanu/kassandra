1e77c8b22c25a4938484ac4eba4149790ee50bbe
Razvan Bordeanu <razvan.bordeanu@misys.com>
2015-10-01 17:00:01 +0300

    bootstrapping

--- src/main/java/com/kassandra/ApplicationSetup.java
+++ src/main/java/com/kassandra/ApplicationSetup.java
@@ -1,9 +1,39 @@
 package com.kassandra;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Scopes;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import com.kassandra.rest.ResponseCorsFilter;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
-@WebServlet(name = "main", urlPatterns = {"/main"})
-public class ApplicationSetup extends HttpServlet {
+public class ApplicationSetup extends GuiceServletContextListener {
 
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new ServletModule() {
+
+            @Override
+            protected void configureServlets() {
+
+                super.configureServlets();
+
+                ResourceConfig resourceConfig = new PackagesResourceConfig("com.kassandra.rest");
+                for (Class<?> resource : resourceConfig.getClasses()) {
+                    bind(resource);
+                }
+
+                bind(JacksonJsonProvider.class).in(Scopes.SINGLETON);
+
+                serve("/app/*").with(GuiceContainer.class);
+
+                filter("/app/*").through(ResponseCorsFilter.class);
+            }
+        }, new ConfigModule());
+
+    }
 }
---
+++ src/main/java/com/kassandra/ConfigModule.java
@@ -0,0 +1,12 @@
+package com.kassandra;
+
+import com.google.inject.AbstractModule;
+
+public class ConfigModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        // bind stuff here
+
+        //bind(DummyService.class).to(DummyServiceImpl.class);
+    }
+}
