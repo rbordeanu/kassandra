e26b0160903db5ef29358c5d98e2de1915fbbf17
lmarines <laurentiu.marinescu@misys.com>
2015-10-12 19:01:40 +0300

    java compiler phase 1

---
+++ compiler/src/test/java/CompileRunTest.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.InvocationTargetException;
+
+import com.kassandra.compiler.JCompiler;
+import com.kassandra.compiler.utils.ResolveClassName;
+
+public class CompileRunTest {
+
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer();
+
+        str.append("import java.util.List;");
+        str.append("import java.util.ArrayList;");
+        str.append("public class HelloWorld {");
+        str.append("  public static void main(String args[]) {");
+        str.append(" List<String> list = new ArrayList<String>(); ");
+        str.append("list.add(\"A\");");
+        str.append("    System.out.println(\"This is in another java file\");");
+        str.append("    System.out.println(list.get(0));");
+        str.append("  }");
+        str.append("}");
+        String code = str.toString();
+
+        String className = ResolveClassName.getClassName(code);
+        String fullClassName = ResolveClassName.getFullClassName(code);
+        JCompiler compiler = new JCompiler("-d", "D:\\hackathon\\kassandra\\compiler\\target"
+                + "\\classes");
+        boolean success = compiler.compile(className, code);
+
+        if (success) {
+            try {
+                Class.forName(fullClassName)
+                        .getDeclaredMethod("main", new Class[] { String[].class })
+                        .invoke(null, new Object[] { null });
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                System.err.println("Load class error: " + e);
+            }
+        }
+        else {
+            System.out.println(compiler.getErrMsg());
+        }
+    }
+}
