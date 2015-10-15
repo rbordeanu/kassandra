cdebcfd76e467c764c5d1b9d8a3c8afc7df5b12d
Razvan Bordeanu <razvan.bordeanu@misys.com>
2015-10-15 23:16:52 +0300

    code editor

--- app/src/main/webapp/js/kassandra/controllers/challengesController.js
+++ app/src/main/webapp/js/kassandra/controllers/challengesController.js
@@ -5,11 +5,13 @@
         .module('app')
         .controller('ChallengesController', ['$scope', '$http', 'urls', function ($scope, $http, urls) {
 
+            $scope.challenges = [];
+
             var init = function() {
 
                 $http.get(urls.BASE + '/task/get').success(
                     function(data){
-                        console.log(data);
+
                     }).error(
                     function(error){
                         console.error(error);
--- app/src/main/webapp/js/kassandra/controllers/userPanelController.js
+++ app/src/main/webapp/js/kassandra/controllers/userPanelController.js
@@ -49,12 +49,12 @@
 
             $scope.currentTab = 'profile';
 
-            var currentStateEqualTo = function(tab) {
+            var currentStateEqualTo = function (tab) {
 
                 return $state.is(tab.route);
             };
 
-            $scope.switchTab = function(tab) {
+            $scope.switchTab = function (tab) {
                 console.log(tab);
                 if (!currentStateEqualTo(tab) && !tab.disabled) {
                     $state.go(tab.route);
@@ -62,14 +62,14 @@
             };
 
             $scope.user = {
-                "_id":"testUserId",
-                "firstName":"Alex",
-                "lastName":"Last",
-                "username":"aturbatu",
-                "email":"aturbatu@misys.com",
-                "password":"passSecre",
-                "gravatarUrl":"https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfp1/v/t1.0-1/c28.116.675.675/s160x160/11822746_1085060741521915_7892215979629432132_n.jpg?oh=d97a2df88054b86d22fa3c5923e890e3&oe=569895CD&__gda__=1451952774_e86712f063ffe82e12d24d4ccb57962f",
-                "admin":true
+                "_id": "testUserId",
+                "firstName": "Alex",
+                "lastName": "Last",
+                "username": "aturbatu",
+                "email": "aturbatu@misys.com",
+                "password": "passSecre",
+                "gravatarUrl": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfp1/v/t1.0-1/c28.116.675.675/s160x160/11822746_1085060741521915_7892215979629432132_n.jpg?oh=d97a2df88054b86d22fa3c5923e890e3&oe=569895CD&__gda__=1451952774_e86712f063ffe82e12d24d4ccb57962f",
+                "admin": true
             };
 
         }]);
