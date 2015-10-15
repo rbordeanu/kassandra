c803bf9df7744729ba740a0a964c3f48682c830e
lmarines <laurentiu.marinescu@misys.com>
2015-10-15 16:55:32 +0300

    compilers

--- rest/src/main/java/com/kassandra/rest/TestRest.java
+++ rest/src/main/java/com/kassandra/rest/TestRest.java
@@ -1,19 +1,25 @@
 package com.kassandra.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Path("/spring")
 @Controller
-@RequestMapping(value = "/test")
 public class TestRest {
 
     @Autowired
     TestService testService;
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String test() {
+    @GET
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody String test() {
         String result = testService.test();
         /*return Response.status(200).entity(result).build();*/
         return testService.test();
