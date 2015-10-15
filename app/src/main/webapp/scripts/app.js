78349062ba487716cd270e473430451c568b1166
Cristian Raducanu <cristian.raducanu@misys.com>
2015-10-15 11:33:58 +0300

    Integration of theme and login token

--- app/src/main/webapp/scripts/app.js
+++ app/src/main/webapp/scripts/app.js
@@ -1,56 +1,63 @@
 (function () {
     'use strict';
 
-    angular
-        .module('app', ['ui.router', 'ngCookies'])
-        .config(config)
-        .run(run);
+    angular.module('app', [
+        'ngStorage',
+        'ngRoute',
+        'angular-loading-bar'
+    ])
+        .constant('urls', {
+            BASE: 'http://localhost:8080/kassandra',
+            BASE_API: 'http://api.localhost:8080/kassandra/v1'
+        })
+        .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/home.html',
+                    controller: 'HomeController'
+                }).
+                when('/signin', {
+                    templateUrl: 'partials/signin.html',
+                    controller: 'HomeController'
+                }).
+                when('/signup', {
+                    templateUrl: 'partials/signup.html',
+                    controller: 'HomeController'
+                }).
+                when('/restricted', {
+                    templateUrl: 'partials/restricted.html',
+                    controller: 'RestrictedController'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
 
-        angular.module('app').directive('socialLogin', function() {
-          return {
-            templateUrl: "socialLogin.html",
-            restrict: "E"
-          }
-        });
-
-    config.$inject = ['$stateProvider', '$urlRouterProvider'];
-    function config($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url:'/home',
-                templateUrl: 'home.html'
-            })
-            .state('login', {
-                url:'/login',
-                controller: 'LoginController',
-                templateUrl: 'login.html',
-                controllerAs: 'vm'
-            })
-            .state('signup', {
-                url:'/signup',
-                controller: 'RegisterController',
-                templateUrl: 'signup.html',
-                controllerAs: 'vm'
-            });
-        $urlRouterProvider.otherwise('/home');
-    }
-
-    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
-    function run($rootScope, $location, $cookieStore, $http) {
-        // keep user logged in after page refresh
-        $rootScope.globals = $cookieStore.get('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+            $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function ($q, $location, $localStorage) {
+                return {
+                    'request': function (config) {
+                        config.headers = config.headers || {};
+                        if ($localStorage.token) {
+                            config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                        }
+                        return config;
+                    },
+                    'responseError': function (response) {
+                        if (response.status === 401 || response.status === 403) {
+                            delete $localStorage.token;
+                            $location.path('/signin');
+                        }
+                        return $q.reject(response);
+                    }
+                };
+            }]);
         }
-
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-            // redirect to login page if not logged in and trying to access a restricted page
-            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
-            var loggedIn = $rootScope.globals.currentUser;
-            if (restrictedPage && !loggedIn) {
-                $location.path('/login');
-            }
+        ]).run(function($rootScope, $location, $localStorage) {
+            $rootScope.$on( "$routeChangeStart", function(event, next) {
+                if ($localStorage.token == null) {
+                    if ( next.templateUrl === "partials/restricted.html") {
+                        $location.path("/signin");
+                    }
+                }
+            });
         });
-    }
-
 })();
