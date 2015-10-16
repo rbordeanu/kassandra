dd701f5657d6952cb326abed7119192b164864ea
Cristian Raducanu <cristian.raducanu@misys.com>
2015-10-16 07:43:23 +0300

    Leaderboard

--- app/src/main/webapp/js/kassandra/controllers/leaderboardController.js
+++ app/src/main/webapp/js/kassandra/controllers/leaderboardController.js
@@ -3,11 +3,22 @@
 
     angular
         .module('app')
-        .controller('LeaderboardController', ['$scope', '$http', function ($scope, $http) {
+        .controller('LeaderboardController', ['$scope', '$http', 'urls', function ($scope, $http, urls) {
 
             var init = function() {
 
-            }
+                $http.get(urls.BASE + '/result/leaderboard').success(
+                    function (result) {
+                        console.log(result);
+                        $scope.board = result;
+
+                    }).error(
+                    function (error) {
+                        console.error(error);
+                    }
+                );
+
+            };
 
             init();
         }]);
